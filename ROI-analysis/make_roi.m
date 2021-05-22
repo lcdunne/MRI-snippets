a529f28398d587fe9211f3158464d6c9a87f9736
lcdunne <56237933+lcdunne@users.noreply.github.com>
2021-05-22 07:59:28 +0100

    Update make_roi.m

--- ROI-analysis/make_roi.m
+++ ROI-analysis/make_roi.m
@@ -2,22 +2,25 @@ function [roi, output_fpath] = make_roi(xyz, radius, roi_name, output_path)
     % Creates a spherical ROI given coordinates --------------------------%
     % PARAMETERS
     % ==========
-    % xyz        : 1d array with x, y, and z values for the sphere centre.
-    % radius     : radius of the sphere in mm
-    % roi_name   : a name for the ROI, optional. If not provided will
-    %               default to `sphere`.
-    % output_path: an optional path to output the sphere to.
-    
-    if ~exist('roi_name', 'var')
-        roi_name = 'sphere';
-    end
+    % xyz        : 'double' (1x3)
+    %     The x, y, and z values to define the sphere centre.
+    % radius     : 'double'
+    %     Radius of the sphere (in mm)
+    % roi_name   : 'char'
+    %     A name/label for the ROI.
+    % output_path: 'char'
+    %     An optional path to output the sphere to.
     
     roi = maroi_sphere(struct('centre', xyz, 'radius', radius));
     
     if exist('output_path', 'var')        
-        roi_label = sprintf('%s_%i_%i_%i_%imm_ROI', roi_name, xyz.', radius);
-        output_fpath = fullfile(output_path, sprintf('%s.mat', roi_label));
+        label = sprintf('%s_%i_%i_%i_%imm_ROI', roi_name, xyz.', radius);
+        % Save as a .mat file
+        output_fpath = fullfile(output_path, sprintf('%s.mat', label));
         saveroi(roi, output_fpath);
+        % % Save as .nii file - only good if want to manually explore in
+        % % spm
+        % save_as_image(roi, fullfile(output_path, sprintf('%s.nii', label)));
     end
     
     fprintf('Created ROI: %s (%i %i %i)\n', roi_name, xyz.');
